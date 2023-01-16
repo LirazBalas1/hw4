07f9bc723231c4ac81f112b18d8a6129d9d993a4
LirazBalas <118938031+LirazBalas1@users.noreply.github.com>
2023-01-16 16:35:18 +0200

    Update main.c

--- main.c
+++ main.c
@@ -20,13 +20,32 @@ int main(){
         else if (c == 'S'){
             int src = -1;
             int dest = -1;
-            scanf("%d, %d\n", &src, &dest);
+            int items = scanf("%d, %d\n", &src, &dest);
+            if (items != 2) {
+                printf("Error: Invalid input. Please enter two integers, separated by a comma.\n");
+                continue;
+            }
+            if (src == dest) {
+                printf("Error: Source and destination nodes cannot be the same.\n");
+                continue;
+            }
             int shortest = shortsPath_cmd(*head,src,dest);
-            printf("Dijsktra shortest path: %d \n", shortest);
+            if (shortest == INF) {
+                printf("Error: Invalid input. The source or destination node does not exist in the graph.\n");
+                continue;
+            }
+            printf("Dijkstra shortest path: %d \n", shortest);
         }
         else if (c == 'T'){
             int tsp = TSP_cmd(*head);
+            if (tsp == INF) {
+                printf("Error: Invalid input. There must be at least two nodes in the graph to calculate TSP.\n");
+                continue;
+            }
             printf("TSP shortest path: %d \n", tsp);
+        }
+        else {
+            printf("Error: Invalid input. Please enter a valid command (A, B, D, S, T).\n");
         }
     }
     deleteGraph_cmd(head);
