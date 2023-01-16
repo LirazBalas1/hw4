2b84daf23bd76152a7994059d33acae72105f90e
LirazBalas <118938031+LirazBalas1@users.noreply.github.com>
2023-01-16 19:13:49 +0200

    Update main.c
    change scanf

--- main.c
+++ main.c
@@ -2,40 +2,51 @@
 #include <stdlib.h>
 #include "graph.h"
 
-int main(){
+int main()
+{
     pnode node = NULL;
     pnode *head = &node;
-    char c= 'v';
+    char c = 'v';
     while (scanf("%c", &c) != EOF)
     {
-        if (c == 'A'){
+        if (c == 'A')
+        {
             build_graph_cmd(head);
         }
-        else if (c == 'B'){
+        else if (c == 'B')
+        {
             insert_node_cmd(head);
         }
-        else if (c == 'D'){
+        else if (c == 'D')
+        {
             delete_node_cmd(head);
         }
-        else if (c == 'S'){
+        else if (c == 'S')
+        {
             int src = -1;
             int dest = -1;
-            int items = scanf("%d, %d\n", &src, &dest);
-            if (items != 2) {
+            scanf("%d", &src);
+            scanf("%d", &dest);
+            if ((src+dest)!= 2)
+            {
                 continue;
             }
-            if (src == dest) {
+            if (src == dest)
+            {
                 continue;
             }
-            int shortest = shortsPath_cmd(*head,src,dest);
-            if (shortest == INF) {
+            int shortest = shortsPath_cmd(*head, src, dest);
+            if (shortest == INF)
+            {
                 continue;
             }
             printf("Dijkstra shortest path: %d \n", shortest);
         }
-        else if (c == 'T'){
+        else if (c == 'T')
+        {
             int tsp = TSP_cmd(*head);
-            if (tsp == INF) {
+            if (tsp == INF)
+            {
                 continue;
             }
             printf("TSP shortest path: %d \n", tsp);
